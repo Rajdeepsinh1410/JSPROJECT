3acb95d94e3383929fda552dafbc812f59c8863a
Rajdeepsinh <dabhirajdipsinh1410@gmail.com>
2024-12-17 20:18:28 +0530

    Done

---
+++ dice.js
@@ -0,0 +1,23 @@
+
+
+function inc(){
+    let value = document.getElementById("value")
+//value.textContent = 1000
+
+let count = 0
+    let min = 1
+    let max = 6
+    let num = Math.round(Math.random()*(max-min))
+    value.textContent = num
+}
+
+function inc1(){
+    let value = document.getElementById("value1")
+//value.textContent = 1000
+
+let count = 0
+    let min = 1
+    let max = 6
+    let num = Math.round(Math.random()*(max-min))
+    value.textContent = num
+}
